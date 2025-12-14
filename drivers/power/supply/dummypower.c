00601877b91596f18f1066a7768923590b463cc3
Mico Antonic <coma.comski@gmail.com>
2025-12-14 22:27:05 +0100

    creating dummy_data struct, sorting global/driver/general related arguments

--- drivers/power/supply/dummypower.c
+++ drivers/power/supply/dummypower.c
@@ -9,11 +9,21 @@
 #include <linux/timer.h>
 #include <linux/jiffies.h>
 #include <linux/workqueue.h>
+#include <linux/container_of.h>
+#include <linux/array_size.h>
 
 #define DUMMY_NAME "Dummy_Driver"
 
-// MiAn check what can go in driver struct
-static enum power_supply_property dummy_prop[] = {
+struct dummy_data {
+	struct timer_list dummy_timer;
+	struct work_struct dummy_work;
+	struct platform_device *dummy_pdev;
+	struct power_supply_config dummy_ps_config;
+	struct power_supply *dummy_ps;
+};
+
+
+static const enum power_supply_property dummy_prop[] = {
 	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_HEALTH,
 	POWER_SUPPLY_PROP_PRESENT,
@@ -27,8 +37,6 @@ static enum power_supply_property dummy_prop[] = {
 	POWER_SUPPLY_PROP_MODEL_NAME,
 	POWER_SUPPLY_PROP_MANUFACTURER,	
 };
-static struct timer_list dummy_timer;
-static struct work_struct dummy_work;
 
 static int dummy_get_prop(struct power_supply *psy, enum power_supply_property psp, union power_supply_propval *val)
 {
@@ -59,30 +67,28 @@ static int dummy_prop_writeable(struct power_supply *psy, enum power_supply_prop
     }
 }
 
-// MiAn check what can go in driver struct
-static struct power_supply_desc dummy_ps_desc = {
-	.name = DUMMY_NAME,
-	.type = POWER_SUPPLY_TYPE_UNKNOWN,
-	.properties = dummy_prop,
-	.num_properties = 12,
-	.get_property = dummy_get_prop,
-	.set_property = dummy_set_prop,
-	.property_is_writeable = dummy_prop_writeable,
-
+static const struct power_supply_desc dummy_ps_desc = {
+    .name = DUMMY_NAME,
+    .type = POWER_SUPPLY_TYPE_UNKNOWN,
+    .properties = dummy_prop,
+    .num_properties = ARRAY_SIZE(dummy_prop),
+    .get_property = dummy_get_prop,
+    .set_property = dummy_set_prop,
+    .property_is_writeable = dummy_prop_writeable,
 };
-static struct power_supply_config dummy_ps_config = {};
-static struct power_supply *dummy_ps;
+
 
 static void timer_callback(struct timer_list *timer)
 {
 	pr_err("Dummy Timer works");
-	schedule_work(&dummy_work);
-	mod_timer(&dummy_timer, jiffies + secs_to_jiffies(1));
+	struct dummy_data *dummy_d = container_of(timer, struct dummy_data, dummy_timer);
+	mod_timer(timer, jiffies + secs_to_jiffies(1));
+	schedule_work(&dummy_d->dummy_work);
 }
 
 static void dummy_working(struct work_struct *work)
 {
-	pr_err("Work queue called\n");
+	pr_err("Work queue called\n");	
 }
 
 static int dummy_probe(struct platform_device *pdev)
@@ -90,28 +96,39 @@ static int dummy_probe(struct platform_device *pdev)
 	// MiAn proper error handling
 	int ret = 0;
 	dev_info(&pdev->dev, "Module probe called.\n");
-	dummy_ps = devm_power_supply_register(&pdev->dev, &dummy_ps_desc, &dummy_ps_config);
-	if(IS_ERR(dummy_ps))
+	struct dummy_data *dummy_d = devm_kzalloc(&pdev->dev, sizeof(struct dummy_data), GFP_KERNEL);
+	if(!dummy_d)
 	{
-		dev_err(&pdev->dev, "Error while dev_power_supply_register:%ld\n", PTR_ERR(dummy_ps));
-		ret = PTR_ERR(dummy_ps);
-		goto reg_err;
+		dev_info(&pdev->dev, "Error while accocating memory: %d\n", ret);
+		return -ENOMEM;
+	}
+	platform_set_drvdata(pdev, (void *)dummy_d);
+
+	dummy_d->dummy_ps = devm_power_supply_register(&pdev->dev, &dummy_ps_desc, &dummy_d->dummy_ps_config);
+	if(IS_ERR(dummy_d->dummy_ps))
+	{
+
+		ret = PTR_ERR(dummy_d->dummy_ps);
+		dev_err(&pdev->dev, "Error while dev_power_supply_register:%d\n", ret);
+		goto alloc_reg_err;
 	}
 	dev_err(&pdev->dev, "Probe success! \n");
 	
-	timer_setup(&dummy_timer, timer_callback, 0);
-	INIT_WORK(&dummy_work, dummy_working);
-	mod_timer(&dummy_timer, jiffies + secs_to_jiffies(1));
-reg_err:
+	timer_setup(&dummy_d->dummy_timer, timer_callback, 0);
+	INIT_WORK(&dummy_d->dummy_work, dummy_working);
+	mod_timer(&dummy_d->dummy_timer, jiffies + secs_to_jiffies(1));
+
+alloc_reg_err:
 	return ret;
 }
 static void dummy_remove(struct platform_device *pdev)
 {
+	struct dummy_data *dummy_d = (struct dummy_data*)platform_get_drvdata(pdev);
 	pr_err("Dummy module remove called.\n");
-	timer_delete_sync(&dummy_timer);
+	timer_delete_sync(&dummy_d->dummy_timer);
+	cancel_work_sync(&dummy_d->dummy_work);
 }
 
-// MiAn check what can go in driver struct
 static const struct of_device_id dummy_of[] = {
 	{ .compatible = "dt_string"},
 	{}
@@ -136,13 +153,15 @@ struct platform_driver dummy_drv = {
 	
 };
 
+/* MiAn: testing purposes, delete */
 struct platform_device *pdev;
 
 static int dummy_ps_init(void)
 {
 	pr_err("Dummy module init started...\n");
 	platform_driver_register(&dummy_drv);
-	// hack, delete
+
+	/* MiAn: testing purposes, delete */
 	pdev = platform_device_register_simple(DUMMY_NAME, -1, NULL, 0);
 	
 	return 0;
